2c6dd34007a0b5ef74ca4816499a8b236b050896
Apanda <Apanda@gap.com>
2024-05-01 11:24:58 +0530

    first commit

---
+++ src/test/java/com/pactice/javacode/JavacodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pactice.javacode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
